33c48729771ae12f085915af1ba7338cf8558dbf
Anish gehlot <anishgehlot@gmail.com>
2023-02-14 23:48:33 +0530

    fix

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
-variable default_location {
-  type = string
+variable "default_location" {
+  type        = string
   description = "Azure Region location"
 }
 
-variable environment {
-  type = string
+variable "environment" {
+  type        = string
   description = "Resource Tags ENVIRONMENT Value"
 }
 
-variable azure_resource_group_name {
-  type = string
+variable "azure_resource_group_name" {
+  type        = string
   description = "Azure Resource Group Name"
 }
 
-variable storage_name {
-  type = string
+variable "storage_name" {
+  type        = string
   description = "Azure Storage Name must be unique and contains only alphanumeric value"
 }
 
-variable storage_account_container {
-  type = string
+variable "storage_account_container" {
+  type        = string
   description = "Azure Storage Blob Private Container Name"
 }
