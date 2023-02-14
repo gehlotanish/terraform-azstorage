441a30fd949f9a74e027da049547954c16425e17
Anish gehlot <anishgehlot@gmail.com>
2023-02-14 23:45:23 +0530

    fix

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.1.0"
+  required_version = "1.1.0"
   required_providers {
     azurerm = {
-      source = "hasicorp/azurerm"
-      version = "~> 2.30"
+      source = "hashicorp/azurerm"
+      version = "2.30.0"
     }
   }
 }
